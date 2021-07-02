71e546c77346942f5c024c51450a8bd7e282e393
Thiago-Cez <thiago.silva.ts804@gmail.com>
2021-07-01 22:49:25 -0300

    finish dropdown - aula 40

--- users/public/javascripts/main.js
+++ users/public/javascripts/main.js
@@ -1,30 +1,72 @@
-const button = document.getElementById("loginBtn");
-console.log(button);
+// const button = document.getElementById("loginBtn");
+// console.log(button);
 
 
+// const form = document.getElementById("form");
+// console.log(form);
+
+// let color = 1;
+
+// setInterval(() => {
+//     const colorSpan = document.getElementById("color");
+//     if (color == 1 ){
+//     button.style.background = "#000";
+//     colorSpan.innerText = "preto";
+//     }
+//     if (color == 2) {
+//         button.style.background ="#930";
+//         colorSpan.innerText = "laranja";
+//     }
+//     if (color == 3 ) {
+//         button.style.background ="#457450";
+//         colorSpan.innerText = "Verde musgo";
+//         color = 1
+//         return 
+//     }
+//     color ++;
+// },1000);
+
+// const background = document.getElementById("background");
+// background.classList.toggle("dark-mode")
+function onClickSubmit(event){
+  event.preventDefault();
+  // pegar valores digitados pelo usuario
+  const inpUser = document.getElementById("user");
+  const inpPassword = document.getElementById("password");
+}
+
+function onClickDarkMode(event){
+  console.log(event)
+  const background = document.getElementById("background");
+  
+  background.classList.toggle("dark-mode")
+
+  if(background.classList.contains("dark-mode")){
+    event.target.innerText = "default";
+  }else event.target.innerText = "dark" 
+}
+
+function onClickDropdown() {
+  const menu = document.querySelector(".dropdown-content");
+  
+  //menu suspenso pode ser dessa maneira comentada
+  // if(menu.style.display == "none"){
+  //   menu.style.display = "block";
+  // }
+  // else menu.style.display = "none";
+
+  menu.classList.toggle("show");// ou dessa forma 
+}
+
+window.onload = function() {
 const form = document.getElementById("form");
-console.log(form);
+form.addEventListener("submit", onClickSubmit);
+const darkModeButton = document.getElementById("darkBtn");
+darkModeButton.addEventListener("click", onClickDarkMode)
 
-let color = 1;
+const dropdownMenu = document.getElementById("dropbtn");
+dropdownMenu.addEventListener("click", onClickDropdown);
 
-setInterval(() => {
-    const colorSpan = document.getElementById("color");
-    if (color == 1 ){
-    button.style.background = "#000";
-    colorSpan.innerText = "preto";
-    }
-    if (color == 2) {
-        button.style.background ="#930";
-        colorSpan.innerText = "laranja";
-    }
-    if (color == 3 ) {
-        button.style.background ="#457450";
-        colorSpan.innerText = "Verde musgo";
-        color = 1
-        return 
-    }
-    color ++;
-},1000);
+}
 
-const background = document.getElementById("background");
-background.classList.toggle("dark-mode")
+
