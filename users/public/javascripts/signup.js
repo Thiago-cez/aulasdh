35994275585dba7b8ef032f75899bcc6aaeca14d
Thiago-Cez <thiago.silva.ts804@gmail.com>
2021-07-10 17:45:25 -0300

    Aula 43

---
+++ users/public/javascripts/signup.js
@@ -0,0 +1,55 @@
+async function onSubmitSignUp(event){
+    event.preventDefault();
+    const user = document.getElementById("userSignup").value;
+    const password = document.getElementById("passwordSignup").value;
+    /*
+         VALIDAR O FORMULARIO
+    */
+
+        //  const response = fetch("http://localhost:3000/users",{
+        //      method: "POST",
+        //      body:JSON.stringify({
+        //          email: user,
+        //          password
+        //      }),
+        //      headers: {
+        //          "content-Type": "application/json"
+        //      }
+        //  });
+        //  const data = await response.json();
+        const { data } = await axios.post("http://localhost:3000/users", {
+            email: user,
+            password
+        });
+         window.alert(`Usuario cadastrado com id ${data.id} `);
+         window.location.href = "/"
+}
+
+async function onBlurCep() {
+    const cepField = document.getElementById("cep");
+    const { data } = await axios.get (`https://viacep.com.br/ws/${cepField.value}/json/`);
+
+    // const ufField = document.getElementById("uf");
+    // const logradouroField = document.getElementById("logradouro");
+    // const bairroField = document.getElementById("bairro");
+    // const cidadeField = document.getElementById("cidade");
+
+    // ufField.value = data.uf;
+    // logradouroField.value = data.logradouro;
+    // bairroField.value = data.bairro;
+    // cidadeField.value = data.cidade;
+  
+     document.getElementById("uf").value = data.uf;
+     document.getElementById("logradouro").value = data.logradouro;
+     document.getElementById("bairro").value = data.bairro;
+     document.getElementById("cidade").value = data.localidade;
+
+}
+
+window.onload = function() {
+    const form = document.getElementById("formSignup");
+    form.addEventListener("submit",onSubmitSignUp);
+
+    const cepField = document.getElementById("cep");
+    cepField.addEventListener("blur", onBlurCep);
+}
