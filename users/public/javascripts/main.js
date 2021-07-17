593e77cdf029568890786b5b7ba09e762f639317
Thiago-Cez <thiago.silva.ts804@gmail.com>
2021-07-16 22:18:23 -0300

    aula 44 completa

--- users/public/javascripts/main.js
+++ users/public/javascripts/main.js
@@ -31,114 +31,114 @@
 // }, 1000);
 
 function onValidateUser() {
-  const inpUser = document.getElementById("user");
-  const user = inpUser.value;
-  const userError = document.getElementById("userError");
-  userError.classList.remove("show");
-  const errors = [];
+    const inpUser = document.getElementById("user");
+    const user = inpUser.value;
+    const userError = document.getElementById("userError");
+    userError.classList.remove("show");
+    const errors = [];
 
-  if (!user) {
-      errors.push({
-          element: userError,
-          message: "Usuário precisa ser informado"
-      });
-      // userError.innerText = "Usuário precisa ser informado";
-      // userError.classList.add("show");
-      // return;
-  }
+    if (!user) {
+        errors.push({
+            element: userError,
+            message: "Usuário precisa ser informado"
+        });
+        // userError.innerText = "Usuário precisa ser informado";
+        // userError.classList.add("show");
+        // return;
+    }
 
-  if (user.indexOf(" ") >= 0) {
-      errors.push({
-          element: userError,
-          message: "O usuário não pode conter espaços em branco"
-      });
-      // userError.innerText = "O usuário não pode conter espaços em branco";
-      // userError.classList.add("show");
-      // return;
-  }
+    if (user.indexOf(" ") >= 0) {
+        errors.push({
+            element: userError,
+            message: "O usuário não pode conter espaços em branco"
+        });
+        // userError.innerText = "O usuário não pode conter espaços em branco";
+        // userError.classList.add("show");
+        // return;
+    }
 
-  if (errors.length > 0) {
-      errors.forEach(error => {
-          error.element.innerText = error.message;
-          error.element.classList.add("show");
-      });        
-  }
+    if (errors.length > 0) {
+        errors.forEach(error => {
+            error.element.innerText = error.message;
+            error.element.classList.add("show");
+        });        
+    }
 
-  return errors.length == 0;
+    return errors.length == 0;
 }
 
 function onValidatePassword() {
-  const inpPassword = document.getElementById("password");
-  const passwordError = document.getElementById("passwordError");
-  passwordError.classList.remove("show");
-  const password = inpPassword.value;
-  const errors = [];
+    const inpPassword = document.getElementById("password");
+    const passwordError = document.getElementById("passwordError");
+    passwordError.classList.remove("show");
+    const password = inpPassword.value;
+    const errors = [];
 
-  if(!password){
-      errors.push({
-          element: passwordError,
-          message: "A senha precisa ser informada"
-      });
-      // passwordError.innerText = "senha precisa ser informado";
-      // passwordError.classList.add("show")
-      // return
-  }
+    if(!password){
+        errors.push({
+            element: passwordError,
+            message: "A senha precisa ser informada"
+        });
+        // passwordError.innerText = "senha precisa ser informado";
+        // passwordError.classList.add("show")
+        // return
+    }
 
-  if (errors.length > 0) {
-      errors.forEach(error => {
-          error.element.innerText = error.message;
-          error.element.classList.add("show");
-      });
-  }
+    if (errors.length > 0) {
+        errors.forEach(error => {
+            error.element.innerText = error.message;
+            error.element.classList.add("show");
+        });
+    }
 
-  return errors.length == 0;
+    return errors.length == 0;
 }
 
 function onClickSubmit(event) {
-  event.preventDefault();
-  
-  if (!onValidatePassword(event) || !onValidateUser(event)) {
-      return;
-  }    
-  console.log('backend');
-  // chamada backend
+    event.preventDefault();
+    
+    if (!onValidatePassword(event) || !onValidateUser(event)) {
+        return;
+    }    
+    console.log('backend');
+    // chamada backend
 }
 
 function onClickDarkMode(event) {
-  console.log(event);
+    console.log(event);
 
-  const background = document.getElementById("background");
-  
-  background.classList.toggle("dark-mode");
+    const background = document.getElementById("background");
+    
+    background.classList.toggle("dark-mode");
 
-  if (background.classList.contains("dark-mode")) {
-      event.target.innerText = "default";
-  }else event.target.innerText = "dark";
+    if (background.classList.contains("dark-mode")) {
+        event.target.innerText = "default";
+    }else event.target.innerText = "dark";
 }
 
 function onClickDropdown() {
-  const menu = document.querySelector(".dropdown-content");
-  // if (menu.style.display == "none") {
-  //     menu.style.display = "block";
-  // }else menu.style.display = "none";
-  menu.classList.toggle("show");
+    const menu = document.querySelector(".dropdown-content");
+    // if (menu.style.display == "none") {
+    //     menu.style.display = "block";
+    // }else menu.style.display = "none";
+    menu.classList.toggle("show");
 }
 
 window.onload = function() {
-  const form = document.getElementById("form");
-  form.addEventListener("submit", onClickSubmit);
-  
-  const background = document.getElementById("background");    
-  background.classList.toggle("dark-mode");
+    const form = document.getElementById("form");
+    form.addEventListener("submit", onClickSubmit);
+    
+    const background = document.getElementById("background");    
+    background.classList.toggle("dark-mode");
 
-  const userInput = document.getElementById("user");
-  userInput.addEventListener("blur", onValidateUser);
+    const userInput = document.getElementById("user");
+    userInput.addEventListener("blur", onValidateUser);
 
-  const passwordInput = document.getElementById("password");
-  passwordInput.addEventListener("blur", onValidatePassword);
-  // const darkModeButton = document.getElementById("darkBtn"); 
-  // darkModeButton.addEventListener("click", onClickDarkMode);
+    const passwordInput = document.getElementById("password");
+    passwordInput.addEventListener("blur", onValidatePassword);
+    // const darkModeButton = document.getElementById("darkBtn"); 
+    // darkModeButton.addEventListener("click", onClickDarkMode);
 
-  // const dropdownMenu = document.getElementById("dropbtn");
-  // dropdownMenu.addEventListener("click", onClickDropdown);
+    // const dropdownMenu = document.getElementById("dropbtn");
+    // dropdownMenu.addEventListener("click", onClickDropdown);
 }
--- users/public/javascripts/signup.js
+++ users/public/javascripts/signup.js
@@ -1,54 +1,53 @@
-async function onSubmitSignUp(event){
+async function onSubmitSignUp(event) {
     event.preventDefault();
     const user = document.getElementById("userSignup").value;
     const password = document.getElementById("passwordSignup").value;
     /*
-         VALIDAR O FORMULARIO
+        VALIDAR O FORMULÁRIO
     */
 
-        //  const response = fetch("http://localhost:3000/users",{
-        //      method: "POST",
-        //      body:JSON.stringify({
-        //          email: user,
-        //          password
-        //      }),
-        //      headers: {
-        //          "content-Type": "application/json"
-        //      }
-        //  });
-        //  const data = await response.json();
-        const { data } = await axios.post("http://localhost:3000/users", {
-            email: user,
-            password
-        });
-         window.alert(`Usuario cadastrado com id ${data.id} `);
-         window.location.href = "/"
+    // const response = await fetch("http://localhost:3000/users", {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         email: user,
+    //         password
+    //     }),
+    //     headers: {
+    //         'Content-Type': 'application/json', 
+    //     }
+    // });
+    // const data = await response.json();
+    const { data } = await axios.post("http://localhost:3000/users", {
+        email: user,
+        password
+    });
+    window.alert(`Usuário cadastrado com id ${data.id}`);
+    window.location.href = "/";
 }
 
 async function onBlurCep() {
     const cepField = document.getElementById("cep");
-    const { data } = await axios.get (`https://viacep.com.br/ws/${cepField.value}/json/`);
-
-    // const ufField = document.getElementById("uf");
-    // const logradouroField = document.getElementById("logradouro");
-    // const bairroField = document.getElementById("bairro");
-    // const cidadeField = document.getElementById("cidade");
+    const { data } = await axios.get(`https://viacep.com.br/ws/${cepField.value}/json/`);
+    
+    // const ufField = document.getElementById("uf"); 
+    // const logradouroField = document.getElementById("logradouro"); 
+    // const bairroField = document.getElementById("bairro"); 
+    // const cidadeField = document.getElementById("cidade"); 
 
     // ufField.value = data.uf;
     // logradouroField.value = data.logradouro;
     // bairroField.value = data.bairro;
     // cidadeField.value = data.cidade;
-  
-     document.getElementById("uf").value = data.uf;
-     document.getElementById("logradouro").value = data.logradouro;
-     document.getElementById("bairro").value = data.bairro;
-     document.getElementById("cidade").value = data.localidade;
-
+    
+    document.getElementById("uf").value = data.uf; 
+    document.getElementById("logradouro").value = data.logradouro; 
+    document.getElementById("bairro").value = data.bairro; 
+    document.getElementById("cidade").value = data.localidade; 
 }
 
 window.onload = function() {
     const form = document.getElementById("formSignup");
-    form.addEventListener("submit",onSubmitSignUp);
+    form.addEventListener("submit", onSubmitSignUp);
 
     const cepField = document.getElementById("cep");
     cepField.addEventListener("blur", onBlurCep);
